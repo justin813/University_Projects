757659764c15601a3630d15efda2b581f1c6d5d2
NagaKanaparthy <nkanaparthyper@live.com>
2015-10-25 23:16:59 -0400

    Finished Program

--- ComputerNetworks/Java/client.java
+++ ComputerNetworks/Java/client.java
@@ -1,1 +1,49 @@
 //Java version of project one.
+/*
+    Author: Nagvarun Kanaparthy
+*/
+import java.io.*;
+import java.net.*;
+
+public class client {
+    public static void main(String[] args) throws IOException {
+        String hostName = "127.0.0.1";
+        int port = 3515;
+        Socket clientSocket = new Socket(hostName, port);
+        //Attempt To open communications between the server and client
+        if(clientSocket != null){
+        try(
+            //Attempt to create the reciving and outputing communications
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ){
+            //if Successful start reading user's input via a buffer stream
+            BufferedReader userInput =
+                new BufferedReader(new InputStreamReader(System.in));
+            //Create
+            String serverResponse;
+            String userResponse;
+            while ((serverResponse = in.readLine()) != null) {
+                System.out.println("Server Response :\n"+ serverResponse +"\n");
+                //the program to exit uses the string "Exit\n"
+                if (serverResponse.equals("Exit")){
+                    System.out.println("Goodbye Program Exiting\n");
+                    break;
+                }
+                //reads user's input and forwards it to the Server
+                if(serverResponse.equals("Select Menu Option")){
+                    userResponse = userInput.readLine();
+                    if (userResponse != null) {
+                        out.println(userResponse);
+                    }
+                }
+            }
+        }
+        catch (IOException e) {
+            System.err.println(e.toString());
+        }
+    }
+    clientSocket.close();
+    }
+}
--- ComputerNetworks/Java/serverMultiThreaded.java
+++ ComputerNetworks/Java/serverMultiThreaded.java
@@ -5,7 +5,6 @@ public class serverMultiThreaded {
     public static void main(String[] args) throws IOException {
         //This is my portNumber
         int portNumber = 3515;
-
         boolean listening = true;
         try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
             while (listening) {
@@ -25,51 +24,100 @@ class projectOneMultiServerThread extends Thread {
         this.socket = socket;
     }
 
-    public void run() {``
-
-        try (
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(
-                new InputStreamReader(
+    public void run() {
+        //Variables
+        PrintWriter out;
+        BufferedReader in;
+        String inputLine;
+        String menu = "1. Host current Date and Time\n2. Host uptime\n3. Host memory use\n4. Host Netstat\n5. Host current users\n6. Host running processes\n7. Quit";
+        //attempt to set up input and output streams
+        try{
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(
+                    new InputStreamReader(
                     socket.getInputStream()));
-        ) {
-            String inputLine, outputLine;
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
-class projectOne{
-    static String netStats;
-    public static String getNetStats(){
-        final String cmd = "netstat";
-        try {
-
-            Process process = Runtime.getRuntime().exec(cmd);
-
-            InputStream in = process.getInputStream();
-
-            File tmp = File.createTempFile("allConnections","txt");
-
-            byte[] buf = new byte[256];
-
-            OutputStream outputConnectionsToFile = new FileOutputStream(tmp);
-
-            int numbytes = 0;
-
-            while ((numbytes = in.read(buf, 0, 256)) != -1) {
-
-                outputConnectionsToFile.write(buf, 0, numbytes);
-
+            //return message to client
+            out.println("Connection Accepted");
+            //loop
+            while(true){
+                //print out menu
+                out.println(menu);
+                //let client know server is done messaging
+                out.println("Select Menu Option");
+                //wait for response
+                inputLine = in.readLine();
+                //respond with correct response
+                out.println(respondData(inputLine));
+                //checkes if cmd was the exit Option
+                if(Integer.parseInt(inputLine) == 7){
+                    break;
+                }
+                //end of response fix
+                out.println("---Response-Compelete---");
             }
-            netStats = String(outputConnectionsToFile.toByteArray(), CharSet.defaultCharSet());
-            System.out.println("File is present at "+tmp.getAbsolutePath());
-
-
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
+            socket.close();
         }
-        return netStats;
+        catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        return;
+    }
+    public static String respondData(String input){
+        int value = Integer.parseInt(input);
+        String response = null;
+        switch(value){
+            case 1:
+                response = getCommandResults("date");
+                break;
+            case 2:
+                response = getCommandResults("uptime");
+                break;
+            case 3:
+                response = getCommandResults("free -m");
+                break;
+            case 4:
+                response = getCommandResults("netstat");
+                break;
+            case 5:
+                response = getCommandResults("w");
+                break;
+            case 6:
+                response = getCommandResults("ps aux | less");
+                break;
+            case 7:
+                response = "Exit";
+                break;
+            default:
+                response = "Not a vaild selection, please choose";
+                break;
+        }
+        return response;
+    }
+    public static String getCommandResults(String cmd){
+        String data = "";
+        String error = "";
+        try {
+            Process process = Runtime.getRuntime().exec(cmd);
+            InputStream in = process.getInputStream();
+            File tmp = File.createTempFile("allConnections","txt");
+            //attempt to initializing buffers
+            BufferedReader stdInput = new BufferedReader(
+                new InputStreamReader(process.getInputStream()));
+            BufferedReader stdError = new BufferedReader(
+                new InputStreamReader(process.getErrorStream()));
+            // read the output from the command
+            String s = null;
+            while ((s = stdInput.readLine()) != null) {
+                data += s + "\n";
+            }
+            while ((s = stdError.readLine()) != null) {
+                error += s;
+            }
+        }
+        catch(Exception e){
+            data = "error";
+        }
+        return data;
     }
 }
